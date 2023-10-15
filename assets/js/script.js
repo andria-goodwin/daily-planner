b3e1a97c7f35a7bb29ed13e29dfae80118aeb20c
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2023-10-15 15:14:27 -0500

    added date using day.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var dateEl = document.getElementById("currentDay");
+
+dateEl.textContent = dayjs().format("dddd, MMMM D")
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
